740e3202eca889f1f269fe2a7f2aa2a5ff63ebe2
丁宗江 <114076268+dingzongjiang@users.noreply.github.com>
2023-05-24 15:37:26 +0800

    测试ssm期末作业的git仓库

---
+++ src/main/java/com/example/endofterm/EndOfTermApplication.java
@@ -0,0 +1,13 @@
+package com.example.endofterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndOfTermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndOfTermApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/endofterm/EndOfTermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endofterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndOfTermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
